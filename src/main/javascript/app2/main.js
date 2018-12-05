c7d41a1dbb0e0d74085e654457a6ac8760b5d19b
akarmanov <alexandrk_@hotmail.com>
2018-12-05 11:16:16 -0500

    Add log

--- src/main/javascript/app2/main.js
+++ src/main/javascript/app2/main.js
@@ -3,6 +3,7 @@ import {Component} from "react";
 import D from '../app/main';
 
 const sayHello = function () {
+        console.log("Hello!");
         console.log("Dynamic React.Component", Component);
         console.log("Dynamic Loading vue");
         D();
